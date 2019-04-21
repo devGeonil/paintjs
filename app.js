471875f63837946dd0d41e255643f78293745a90
devGeonil <jgi92@naver.com>
2019-04-22 00:48:22 +0900

    make line event

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+
+canvas.width = 600;
+canvas.height = 600;
+ctx.strokeStyle="#2c2c2c";
+ctx.lineWidth = 2.5;
+
 
 let painting = false;
 
@@ -6,10 +13,20 @@ function stopPainting(){
   painting = false;
 }
 
+function startPainting(){
+  painting = true;
+}
+
 function onMouseMove(event){
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x,y)
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+  }else{
+    ctx.lineTo(x,y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event){
@@ -23,8 +40,7 @@ function onMouseUp(event){
 
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvae.addEventListener("mouseleave", stopPainting);
-
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
